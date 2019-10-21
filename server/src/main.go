2cb784ca4bb51bd617d6627e8bfb93999193e56f
Sergey Shpak <sshpak@wallix.com>
2019-10-21 23:09:51 +0200

    feat(server): add cors mechanism to accept POST requests with JSON

--- server/src/main.go
+++ server/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"github.com/SergeyShpak/owngame/server/src/handlers"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
